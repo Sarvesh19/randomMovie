f3a158482d414bd6f8c2b0a8fea175a91e831c2f
SARVESH YADAV <sarveshyadav@SARVESHs-MacBook-Air.local>
2022-08-16 17:38:26 +0530

    message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,17 +19,28 @@ export interface PeriodicElement {
 })
 export class AppComponent {
 	dataSource:any = [];
+	starRating : any = 4;
+	loading : boolean = true;
  displayedColumns: string[] = [ 'image','title', 'weight', 'symbol'];
  // dataSource = ELEMENT_DATA;
 
 constructor(private appService: RandomMovie) {
+	this.loading  = true;
 	this.initUserList();
 
 }
 
   initUserList() {
+  		this.loading  = true;
     this.appService.getRandomMovie().subscribe(response => {
+    	this.loading  = false;
       this.dataSource = response;
     });
+  }
+
+  goto(title : string){
+  	let url : string  = "https://www.google.com/search?q="+title;
+    window.open(url, "_blank");
+
   }
 }
